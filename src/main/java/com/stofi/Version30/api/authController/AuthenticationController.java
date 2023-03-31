c4525d6249634c222565567796e3fe6277a1b009
Stofi09 <kristof.solak@gmail.com>
2023-03-31 17:33:19 +0200

    Reg API added

---
+++ src/main/java/com/stofi/Version30/api/authController/AuthenticationController.java
@@ -0,0 +1,38 @@
+package com.stofi.Version30.api.authController;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    /** The user service. */
+    private UserService userService;
+
+    /**
+     * Spring injected constructor.
+     * @param userService
+     */
+    public AuthenticationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * Post Mapping to handle registering users.
+     * @param registrationBody The registration information.
+     * @return Response to front end.
+     */
+    @PostMapping("/register")
+    public ResponseEntity registerUser(@Valid @RequestBody RegistrationBody registrationBody) {
+        try {
+            userService.registerUser(registrationBody);
+            return ResponseEntity.ok().build();
+        } catch (UserAlreadyExistsException ex) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+}
